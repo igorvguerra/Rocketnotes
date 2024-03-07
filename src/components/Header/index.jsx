c66b5edc8969c3eee5c72c0e30711639ef879b69
Igor <igor.vguerra@hotmail.com>
2024-03-07 14:21:19 +0000

    working on the header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,23 @@
+import { RiShutDownLine } from 'react-icons/ri';
+
+import { Container, Profile, Logout } from './styles';
+
+export function Header() {
+    return (
+        <Container>
+            <Profile>
+                <img src="https://github.com/igorvguerra.png" alt='Foto do usuário.'/>
+
+                <div>
+                    <span>Bem-vindo</span>
+                    <strong>Igor Guerra</strong>
+                </div>
+            </Profile>
+
+            <Logout>
+                <RiShutDownLine/>
+            </Logout>
+
+        </Container>
+    );
+}
