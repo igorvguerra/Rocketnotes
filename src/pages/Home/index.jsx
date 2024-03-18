4ec41fa56115f676699aa6271f3f2634d2e0ea7f
Igor <igor.vguerra@hotmail.com>
2024-03-18 19:37:50 +0000

    creating the menu and newnote

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,9 @@
+import { FiPlus } from 'react-icons/fi'
+
 import { Container, Brand, Menu, Search, Content, NewNote} from './styles';
 
 import { Header } from '../../components/Header';
+import { ButtonText } from '../../components/ButtonText';
 
 export function Home() {
     return (
@@ -12,7 +15,9 @@ export function Home() {
             <Header/>
 
             <Menu>
-
+                <li><ButtonText title="Todos" $isactive /></li>
+                <li><ButtonText title="React"/></li>
+                <li><ButtonText title="Nodejs"/></li>
             </Menu>
 
             <Search>
@@ -23,7 +28,10 @@ export function Home() {
 
             </Content>
 
-            <NewNote></NewNote>
+            <NewNote>
+                <FiPlus />
+                    Criar nota
+            </NewNote>
         </Container>
     );
 };
