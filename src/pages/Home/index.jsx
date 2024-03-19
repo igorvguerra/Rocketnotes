bfb9ed8d4e75a8e06a8f22188acdf9befc65a17e
Igor <igor.vguerra@hotmail.com>
2024-03-19 12:58:57 +0000

    creating another component. Note

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,12 @@
-import { FiPlus } from 'react-icons/fi'
+import { FiPlus, FiSearch } from 'react-icons/fi'
 
 import { Container, Brand, Menu, Search, Content, NewNote} from './styles';
 
 import { Header } from '../../components/Header';
 import { ButtonText } from '../../components/ButtonText';
+import { Input } from '../../components/Input';
+import { Section } from '../../components/Section';
+import { Note } from '../../components/Note';
 
 export function Home() {
     return (
@@ -21,11 +24,18 @@ export function Home() {
             </Menu>
 
             <Search>
-
+                <Input placeholder ="Pesquisar pelo título" icon={FiSearch} />
             </Search>
 
             <Content>
-
+                <Section title="Minhas notas">
+                    <Note data={{ title: 'React',
+                tags:[
+                    {id: '1', name: 'react'},
+                    {id: '2', name: 'rocketseat'}
+                ]
+                }} />
+                </Section>
             </Content>
 
             <NewNote>
