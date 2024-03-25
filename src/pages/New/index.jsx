d80d31a487b77822987355214006de64b5ce674b
Igor <igor.vguerra@hotmail.com>
2024-03-25 21:29:42 +0000

    finished the New Page and starting with the app routes

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,7 +1,9 @@
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
 import { Textarea } from '../../components/Textarea';
-
+import { NoteItem } from '../../components/NoteItem';
+import { Section } from '../../components/Section';
+import { Button } from '../../components/Button';
 
 import { Container, Form } from './styles';
 
@@ -19,6 +21,18 @@ export function New() {
 
                     <Input placeholder="Título" />
                     <Textarea placeholder="Observações" />
+
+                    <Section title="Links Úteis" >
+                        <NoteItem value="https://rocketseat.com.br" />
+                        <NoteItem isNew placeholder="Novo link" />
+                    </Section>
+                    <Section title="Marcadores">
+                        <div className='tags'>
+                            <NoteItem value="react" />
+                            <NoteItem isNew placeholder="Nova tag" />
+                        </div>
+                    </Section>
+                    <Button title="Salvar" />
                 </Form>
             </main>
         </Container>
