82bd6951fad8421b21f95c26c3e1297d9a4df38e
Igor <igor.vguerra@hotmail.com>
2024-03-25 21:45:46 +0000

    Routes added

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
-import { New } from './pages/New';
+import { Routes } from './routes';
 import theme from './styles/theme.js';
 import GlobalStyles from './styles/global.js';
 
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <New />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>,
 )
