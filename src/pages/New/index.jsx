ae479972e32c1107fd86427cb901f176ac393636
Igor <igor.vguerra@hotmail.com>
2024-03-25 20:19:24 +0000

    new component added, Textarea

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,11 +1,26 @@
 import { Header } from '../../components/Header';
+import { Input } from '../../components/Input';
+import { Textarea } from '../../components/Textarea';
 
-import { Container } from './styles';
+
+import { Container, Form } from './styles';
 
 export function New() {
     return(
         <Container>
             <Header />
+
+            <main>
+                <Form>
+                    <header>
+                        <h1>Criar Nota</h1>
+                        <a href="/">voltar</a>
+                    </header>
+
+                    <Input placeholder="Título" />
+                    <Textarea placeholder="Observações" />
+                </Form>
+            </main>
         </Container>
 
     );
