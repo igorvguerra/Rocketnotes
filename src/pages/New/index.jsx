0c78e3da99be920bf253e7352ae21a37a965c8db
Igor <igor.vguerra@hotmail.com>
2024-05-19 20:09:41 +0100

    adding and deleting tags from notes

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -15,6 +15,9 @@ export function New() {
     const [links, setLinks] = useState([]);
     const [newLink, setNewLink] = useState("");
 
+    const [tags, setTags] = useState([]);
+    const [newTag, setNewTag] = useState("");
+
     function handleAddLink(){
         setLinks(prevState => [...prevState, newLink])
         setNewLink("");
@@ -22,6 +25,15 @@ export function New() {
 
     function handleRemoveLink(deleted){
         setLinks(prevState => prevState.filter(link => link !== deleted));
+    }
+
+    function handleAddTag(){
+        setTags(prevState => [...prevState, newTag]);
+        setNewTag("");
+    }
+
+    function handleRemoveTag(deleted) {
+        setTags(prevState => prevState.filter(tag => tag !== deleted));
     }
 
     return(
@@ -59,8 +71,22 @@ export function New() {
                     </Section>
                     <Section title="Marcadores">
                         <div className='tags'>
-                            <NoteItem value="react" />
-                            <NoteItem isNew placeholder="Nova tag" />
+                            {
+                                tags.map((tag, index) => (
+                                    <NoteItem 
+                                    key={String(index)}
+                                    value={tag} 
+                                    onClick={() => handleRemoveTag(tag)}
+                                    />
+                                ))
+                            }
+                            <NoteItem 
+                            isNew 
+                            placeholder="Nova tag" 
+                            onChange={e => setNewTag(e.target.value)}
+                            value={newTag}
+                            onClick={handleAddTag}
+                            />
                         </div>
                     </Section>
                     <Button title="Salvar" />
