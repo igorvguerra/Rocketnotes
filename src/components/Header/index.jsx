0c1b59350daa222bace4eade19bb0958d03bc5b0
Igor <igor.vguerra@hotmail.com>
2024-05-28 11:40:20 +0100

    trying to resize the avatar uploads

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,6 +2,7 @@ import { RiShutDownLine } from 'react-icons/ri';
 import { api } from '../../services/api';
 import avatarPlaceHolder from '../../assets/avatar_placeholder.svg';
 import { useNavigate } from 'react-router-dom';
+import Resizer from 'react-image-file-resizer';
 
 
 import { useAuth } from '../../hooks/auth';
@@ -19,11 +20,22 @@ export function Header() {
     }
 
     const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceHolder;
+
+    const resizedAvatar = (avatarUrl) => {
+        Resizer.imageFileResizer(
+            avatarUrl,
+            400,
+            400,
+            'JPEG',
+            100,
+            0,
+        )
+    };
     
     return (
         <Container>
             <Profile to="/profile">
-                <img src={avatarUrl} alt={user.name} />
+                <img src={resizedAvatar} alt={user.name} />
 
                 <div>
                     <span>Bem-vindo</span>
